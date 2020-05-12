ab94216339cd8df0a22c0bf9dc132c5cba397936
jaimin1224 <marfatiajaimin@gmail.com>
2020-05-12 19:18:35 -0400

    - Added model and some mock data

---
+++ CommandLineSoln/CommandLine.API/Data/ICommanderRepo.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandLine.API.Data
+{
+    public interface ICommanderRepo
+    {
+        IEnumerable<Command> GetAppCommands();
+        Command GetCommandById(int id);
+    }
+}
---
+++ CommandLineSoln/CommandLine.API/Data/MockCommanderRepo.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandLine.API.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Boil an Egg", Line = "Boil Water", Platform = "Cooking" },
+                new Command { Id = 1, HowTo = "Cut bread", Line = "Get knife", Platform = "Chopping board" },
+                new Command { Id = 2, HowTo = "Make cup of tea", Line = "Place teabag", Platform = "Kettle" }
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an Egg", Line = "Boil Water", Platform = "Cooking" };
+        }
+    }
+}
